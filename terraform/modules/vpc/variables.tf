68478bcc1fef67333f1215c3d1bad8d595812425
SreekuttanCL <sreekuttancl12@gmail.com>
2026-02-01 16:33:50 -0500

    Added VPC module

---
+++ terraform/modules/vpc/variables.tf
@@ -0,0 +1,13 @@
+variable "project_name" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
